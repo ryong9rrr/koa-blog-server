c9bb67b72c350fab99cad9957a279f9b051adac8
yong <tkddbs1644@gmail.com>
2022-10-06 01:49:02 +0900

    config: mongoose 연결

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import Koa from 'koa'
 import Router from '@koa/router'
 import bodyParser from 'koa-bodyparser'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 import api from './api'
+dotenv.config()
+
+const { PORT, MONGO_URI } = process.env
+
+mongoose
+  .connect(MONGO_URI as string)
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((e) => console.error(e))
 
 const app = new Koa()
 const router = new Router()
@@ -11,6 +23,8 @@ router.use('/api', api.routes())
 app.use(bodyParser())
 app.use(router.routes()).use(router.allowedMethods())
 
+const port = PORT || 4000
+
 app.listen(4000, () => {
-  console.log('Listening to port 4000')
+  console.log('Listening to port %d', port)
 })
