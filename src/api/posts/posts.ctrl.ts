ebe25baaba4abf5dff5b994f7fcfa1433b32a9ad
yong <tkddbs1644@gmail.com>
2022-10-08 01:08:41 +0900

    feat: 포스트 작성 시 사용자 정보를 넣는 기능 추가

--- src/api/posts/posts.ctrl.ts
+++ src/api/posts/posts.ctrl.ts
@@ -38,7 +38,8 @@ export const write = async (ctx: Context) => {
   const post = new Post({
     title,
     body,
-    tags
+    tags,
+    user: ctx.state.user
   })
   try {
     await post.save()
